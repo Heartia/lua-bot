f7c3c7ea06de68318aa447d2444f37cb373773cc
DevInDaDen <Patnaterson2048@gmail.com>
2019-07-20 22:16:33 -0500

    Update invitebot.lua

--- invitebot.lua
+++ invitebot.lua
@@ -1,5 +1,5 @@
-botName = "Hollobo64#2371" -- your bot's name
-botPw = "2KnightsOfTheHollow72" -- your bot's password
+botName = "Hollobot#4908" -- your bot's name
+botPw = "2KnightsOfTheHollow48" -- your bot's password
 ownerID = "101454901" -- ownerID
 key = "pcAVfk2e-Mk3plwXgE284gFW6iFfmQ9gpyVp8U9g4Mb-BqKSdED3QIvXURbRDr" -- key
 chat = "blbots" -- If things get a little out of control in the #blbots chat, try changing this value to something else.

5110d367f59179792857bed6caca46e86afa933f
DevInDaDen <Patnaterson2048@gmail.com>
2019-09-28 13:26:12 -0500

    Update invitebot.lua

--- invitebot.lua
+++ invitebot.lua
@@ -352,11 +352,11 @@ client:on(
                                             end
                                         )
                                     else
-                                        print("There was no one in that room. The bot is now available again.")
-                                        client:sendChatMessage(
-                                            chat,
-                                            "There was no one in that room. The bot is now available again."
-                                        )
+                                        --print("There was no one in that room. The bot is now available again.")
+                                        --client:sendChatMessage(
+                                        --    chat,
+                                        --    "There was no one in that room. The bot is now available again."
+                                        --)
                                     end
                                 end
                             )
